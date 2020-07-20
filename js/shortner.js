c996262403f4058f4de865dd5065ea12f8d8b04f
Tushar Kashyap <kashyap.tushar15@gmail.com>
2020-07-20 13:58:03 +0530

    Complete desktop version

--- js/shortner.js
+++ js/shortner.js
@@ -1,4 +1,4 @@
-async function shortUrl() {
+async function shortenUrl() {
   let link = document.getElementById('url').value;
   let myBody = {
     url: link,
@@ -11,26 +11,51 @@ async function shortUrl() {
     body: JSON.stringify(myBody),
   };
   let request = new Request('https://rel.ink/api/links/', initObject);
+  try {
+    let response = await fetch(request).then((res) => res.json());
+    let shortenedLinkResponse = `https://rel.ink/${response.hashid}`;
 
-  let response = await fetch(request).then((res) => res.json());
-  let shortenedLinkResponse = `https://rel.ink/${response.hashid}`;
+    // Updating the DOM with results
+    let allShortenedLinks = document.getElementById('allShortenedLinks');
 
-  let allShortenedLinks = document.getElementById('allShortenedLinks');
+    let shortenedLinkContainer = document.createElement('div');
+    shortenedLinkContainer.className = 'shortenedLinkContainer';
 
-  let shortenedLinkContainer = document.createElement('div');
-  shortenedLinkContainer.className = 'shortenedLinkContainer';
+    let enteredlink = document.createElement('span');
+    enteredlink.textContent = link;
+    shortenedLinkContainer.appendChild(enteredlink);
 
-  let enteredlink = document.createElement('span');
-  enteredlink.textContent = link;
-  shortenedLinkContainer.appendChild(enteredlink);
+    let copyLink = document.createElement('div');
+    copyLink.innerHTML = `<a href="${shortenedLinkResponse}" target="_blank">${shortenedLinkResponse}</a>
+                        <button class="copyBtn">Copy</button>`;
 
-  let copyLink = document.createElement('div');
-  copyLink.innerHTML = `<a href="${shortenedLinkResponse}" target="_blank">${shortenedLinkResponse}</a>
-                        <button>Copy</button>`;
+    shortenedLinkContainer.appendChild(copyLink);
 
-  shortenedLinkContainer.appendChild(copyLink);
-
-  allShortenedLinks.appendChild(shortenedLinkContainer);
+    allShortenedLinks.appendChild(shortenedLinkContainer);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-document.getElementById('shortUrl').addEventListener('click', shortUrl);
+function copyShortenedLink(e) {
+  let range = document.createRange();
+  range.selectNode(e.target.previousElementSibling);
+  window.getSelection().removeAllRanges(); // clear current selection
+  window.getSelection().addRange(range); // to select text
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges(); // to deselect
+
+  e.target.textContent = 'Copied!';
+  e.target.style.backgroundColor = 'hsl(257, 27%, 26%)';
+  setTimeout(() => {
+    e.target.textContent = 'Copy';
+    e.target.style.backgroundColor = 'hsl(180, 66%, 49%)';
+  }, 3000);
+}
+
+document.getElementById('shortenUrl').addEventListener('click', shortenUrl);
+document.getElementById('allShortenedLinks').addEventListener('click', (e) => {
+  if ((e.target.className = 'copyBtn')) {
+    copyShortenedLink(e);
+  }
+});
